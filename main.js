20d27c2262ba8923fb3a96641b039eb924bddbbb
newsty <xadichaermatova19@gmail.com>
2023-06-15 20:00:14 +0500

    vazifa-26

---
+++ main.js
@@ -0,0 +1,136 @@
+// 1-masala
+// let a=-10;
+// if (a>0){
+//     console.log(true);
+// }
+// else if(a<0){
+//     console.log(false);
+// }
+// else{
+//     console.log('0 ga teng');
+// }
+
+// 2-masala toq son topish
+// let b=-10;
+// if (b % 2 !=0){
+//     console.log(true);
+// }
+
+// else{
+//     console.log(false);
+// }
+ 
+// 3-masala  juft son topish
+// let c=-10;
+// if (c % 2 ==0){
+//     console.log(true);
+// }
+
+// else{
+//     console.log(false);
+// }
+
+// 4-masala
+// let son1=12,son2=5;
+// if (son1>0 && son2>0){
+//     console.log(true)
+// }
+// else{
+//     console.log(false);
+// }
+
+// 5-masala
+// let son3=4,son4=2,son5=1;
+// if(son3>son4 && son4>son5){
+//     console.log(true);
+// }
+// else {
+//     console.log(false);
+// }
+
+// 6-masala
+// let num1=1,num2=2,num3=3;
+// if(num2 < num3 && num2 > num3){
+//    console.log("num2 o'rta"); 
+// }
+// else if(num2 > num3 && num2 < num3){
+//     console.log("num2 o'rta"); 
+//  }
+//  else{
+//     console.log("noto'g'ri");
+//  }
+
+// 7-masala
+// let day=7;
+// switch(day){
+//     case 1:
+//     console.log("Monday");
+//     break;
+//     case 2:
+//     console.log("Tuesday");
+//     break;
+//     case 3:
+//     console.log("Wednesday");
+//     break;
+//     case 4:
+//     console.log("Thuesday");
+//     break;
+//     case 5:
+//     console.log("Friday");
+//     break;
+//     case 6:
+//     console.log("Saturday");
+//     break;
+//     case 7:
+//     console.log("Sunday");
+//     break;
+//     default:
+//         console.log("Bunday kun mavjud emas");
+// }
+
+// 8-masala
+// let num6=90;
+// switch(num6){
+//     case 94:
+//         console.log("USEL");
+//         break;
+//     case 90:
+//         console.log("BELINE");
+//         break; 
+//      case 93:
+//         console.log("UMS");
+//         break; 
+//     default:
+//         console.log("Bunday kompaniya mavjud emas");       
+// }
+
+// 9-masala
+// let ball=5
+// switch(ball){
+//     case 5:
+//         console.log("Sizning stipendiyangiz 900 000 so'm");
+//         break;
+//     case 4:
+//         console.log("Sizning stipendiyangiz 700 000 so'm");
+//         break; 
+//     case 3:
+//         console.log("Sizning stipendiyangiz 500 000 so'm");
+//         break;
+//     default:
+//         console.log("Sizning balingiz stipendiya olish uchun yetarli emas");    
+// }
+
+// 10-masala
+// let gender=woman;
+// switch(gender){
+//     case 'woman':
+//         console.log('Ayol');
+//         break;
+//     case 'man':
+//         console.log('Erkak');
+//         break;  
+//         default:
+//             console.log("Noto'g'ri kiritdingiz");    
+// }
+
+// 11-masala
